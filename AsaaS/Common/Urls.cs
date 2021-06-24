5a33457e6ab510c4ae487fd36d452e7c859fbc1e
Diego Garcia <diego.garcia@hqsoftwares.com.br>
2021-06-24 09:49:05 -0300

    ! Fixed production URL.

--- AsaaS/Common/Urls.cs
+++ AsaaS/Common/Urls.cs
@@ -3,6 +3,6 @@ namespace AsaaS.Common
     public static class Urls
     {
         public const string SandboxApiUrl = "https://sandbox.asaas.com/api/v3/";
-        public const string ApiUrl = "https://www.asaas.com.br/api/v3/";
+        public const string ApiUrl = "https://www.asaas.com/api/v3/";
     }
 }
